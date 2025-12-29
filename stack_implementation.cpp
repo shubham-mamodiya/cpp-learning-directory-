af7c99057c9b9900290b54eb6b40f90073332c59
Shubham Mamodiya <shubham.mamodiya.dev@gmail.com>
2025-12-29 17:51:57 +0530

    added complete array based stack implementation for words

--- stack_implementation.cpp
+++ stack_implementation.cpp
@@ -14,12 +14,12 @@
  */
 class ArrayStack {
 private:
-  std::string *m_stack = new std::string[10]{};
-  std::size_t m_capacity{10};
+  std::size_t m_capacity{16}; // why 16? Because 16 times 2 or divided by 2 is
+                              // always even. But odd is also fine
+  std::string *m_stack = new std::string[m_capacity]{};
   std::size_t m_top{};
 
-  void resize() {
-    std::size_t m_new_capacity = m_capacity * 2;
+  void resize(size_t m_new_capacity) {
     std::string *m_new_stack = new std::string[m_new_capacity];
 
     for (std::size_t index = 0; index < m_top; ++index) {
@@ -32,9 +32,11 @@ private:
   }
 
 public:
+  ~ArrayStack() { delete[] m_stack; }
+
   void push(std::string_view item) {
     if (m_top >= m_capacity) {
-      resize();
+      resize(m_capacity * 2);
     }
     m_stack[m_top++] = item;
   }
@@ -44,23 +46,35 @@ public:
       throw std::runtime_error("Stack is empty!");
     }
 
+    if (m_top > 0 && m_top <= (m_capacity / 2)) {
+      resize(m_capacity / 2);
+    }
     return std::move(
         m_stack[--m_top]); // because top is a count of elements not index
   }
 
   bool is_empty() const { return m_top == 0; }
 
-  auto size() const { return m_top; }
+  auto size() const { return m_capacity; }
 };
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
   ArrayStack words;
   std::string input{};
-  int count{-1};
+  int count{1};
+  std::cout << "Enter ('-' to pop, '=' for size) \n";
   while (true) {
-    std::cout << "Enter a word " << ++count << ": ";
+    std::cout << "\nEnter a word " << count << ": ";
     std::cin >> input;
-    words.push(input);
+    if (input == "-") {
+      std::cout << "\n Popped Item: " << words.pop() << "\n";
+      --count;
+    } else if (input == "=") {
+      std::cout << "\n Size: " << words.size() << "\n";
+    } else {
+      words.push(input);
+      ++count;
+    }
   }
   return 0;
 }
