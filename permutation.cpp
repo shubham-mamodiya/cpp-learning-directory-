f954af8b0c26e7a411142688d61010cc2528a7ed
Shubham Mamodiya <shubham.mamodiya.dev@gmail.com>
2026-01-01 15:01:29 +0530

    Started permutatuion project and implementing queue data structure that can be created with any primitive data type

---
+++ permutation.cpp
@@ -0,0 +1,2 @@
+/* This is an implementation of queue that can be created with mostly any
+ * Primitive data types*/
---
+++ queue.h
@@ -0,0 +1,8 @@
+
+/* This is an implementation of queue that can be created with mostly any
+ * Primitive data types*/
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#endif // QUEUE
