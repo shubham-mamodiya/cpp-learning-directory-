249423fe2daedd2c1d2107702bf02c59831f3173
Shubham Mamodiya <shubham.mamodiya.dev@gmail.com>
2026-01-02 22:09:04 +0530

    Learning Deque

---
+++ deque.h
@@ -0,0 +1,13 @@
+
+/* This is an implementation of queue that can be created with mostly any
+ * Primitive data types*/
+
+#ifndef DEQUE_H
+#define DEQUE_H
+
+template <typename T> class Deque {
+private:
+public:
+};
+
+#endif // DEQUE_H
--- queue.h
+++
@@ -1,8 +0,0 @@
-
-/* This is an implementation of queue that can be created with mostly any
- * Primitive data types*/
-
-#ifndef QUEUE_H
-#define QUEUE_H
-
-#endif // QUEUE
