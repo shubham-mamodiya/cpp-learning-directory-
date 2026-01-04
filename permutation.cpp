05687d74e7d227c0de0e0255853d7cf386908a27
Shubham Mamodiya <shubham.mamodiya.dev@gmail.com>
2026-01-04 15:51:49 +0530

    testing deque

--- permutation.cpp
+++ permutation.cpp
@@ -1,2 +1,12 @@
-/* This is an implementation of queue that can be created with mostly any
- * Primitive data types*/
+#include "deque.h"
+#include <cstddef>
+
+int main(int argc, char *argv[]) {
+  std::size_t size{10};
+  Deque<std::size_t> container{};
+  for (std::size_t x; x < size; ++x) {
+    container.push_front(x);
+  }
+  std::cout << container.get_size() << std::endl;
+  return 0;
+}
