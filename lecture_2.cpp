bd7d3c2f5088ebb261e860e6bf896df201fe25e2
Shubham Mamodiya <shubham.mamodiya.dev@gmail.com>
2026-02-07 18:22:00 +0530

    working merge sort implementation

---
+++ lecture_2.cpp
@@ -0,0 +1,33 @@
+// program objective: print your introduction , your institute introduction
+#include <iostream>
+
+// simple interest formula r * p * t / 100
+int main() {
+    std::cout << "Printing My introduction and My institute introduction\n";
+    std::cout << "Here comes my introduction.\n";
+
+    std::cout << "\nName: Shubham Mamodiya\nAddress: 21, Near post office Dikadla, SAMALKHA BL. "
+                 "Panipat. Haryana, India.\nCurrent profession: Student\nLearning At: Arya Post "
+                 "Graduation college, Panipat\nShort & Long term Gole: Just spend my daily life "
+                 "doing what I enjoy doing.\nThings that I enjoy(Hobies): Reading books, writing "
+                 "smart code, maths(kind of new interest), watching Animated movies And so on.\n";
+
+    std::cout << "\nHere comes institute introduction.\n";
+    std::cout
+        << "Instutute: Arya college Panipat\nAddress: G.T.Road, opposite Bus Stand, Panipat, "
+           "Haryana - 132103, India.\nPhone: +91 0180 269 5733\nOfficial Website: "
+           "aryapgcollege.ac.in\nEmail (general/official): principalaryapgcollege@gmail.com\n"
+           "About the College: Established in 1954, this is one of the older and well - known "
+           "general degree colleges in Haryana. It is often referred to as Arya Post Graduate "
+           "College. Affiliated with Kurukshetra University, Haryana. Recognized by UGC and "
+           "accredited by NAAC. Campus size is around 15 acres with facilities like classrooms, "
+           "labs, library,and sports amenities.";
+
+    std::cout << "\nCourses & Academics\nThe college offers a range of undergraduate and "
+                 "postgraduate programs, including:\n\tBachelor of Arts (B.A)\n\tBachelor of "
+                 "Science (B.Sc)\n\tBachelor of Commerce (B.Com)\n\tBachelor of Computer "
+                 "Applications (BCA)\n\tBachelor of Business Administration (BBA)\n\tPost Graduate "
+                 "Diploma in Computer Applications (PGDCA)\n\tMaster of Arts (M.A)\n\tMaster of "
+                 "Commerce (M.Com)\n\tMaster of Science (M.Sc)";
+    return 0;
+}
--- merge_sort_implementation.cpp
+++ merge_sort_implementation.cpp
@@ -1,49 +1,44 @@
+#include "random_mt.h"
 
 #include <cassert>
+#include <cstddef>
 #include <iostream>
 #include <type_traits>
 #include <vector>
 
-using namespace std;
 template <typename T> class Sort {
-    static_assert(is_arithmetic_v<T>,
+    static_assert(std::is_arithmetic_v<T>,
                   "!Sort object only intended to work on premetive data types.");
 
   private:
-    // nothing yet remove if you want
-
-    vector<T> m_data{};
-    vector<T> aux{};
+    std::vector<T> m_data{};
+    std::vector<T> aux{};
 
     void merge(int lo, int mid, int hi) {
-        assert(lo > 0 || hi > 0 || hi < static_cast<int>(m_data.capacity()) ||
-               lo < static_cast<int>(m_data.capacity()));
+        if (lo >= 0 && hi < static_cast<int>(m_data.size()) && lo <= hi) {
 
-        mid += 1;
-
-        // assert(!is_sorted(m_data, lo, mid));
-        //
-        // assert(!is_sorted(m_data, mid + 1, hi));
-
-        for (int k = lo; k <= hi; k++) {
-            aux[k] = m_data[k];
-        }
-
-        int i = lo, j = mid;
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid) {
-                m_data[k] = aux[j++];
-            } else if (j > hi) {
-                m_data[k] = aux[i++];
-            } else if (less(aux[j], aux[i])) {
-                m_data[k] = aux[j++];
-            } else {
-                m_data[k] = aux[i++];
+            for (int k = lo; k <= hi; k++) {
+                aux[k] = m_data[k];
             }
+
+            int i = lo, j = mid + 1;
+            for (int k = lo; k <= hi; k++) {
+                if (i > mid) {
+                    m_data[k] = aux[j++];
+                } else if (j > hi) {
+                    m_data[k] = aux[i++];
+                } else if (less(aux[j], aux[i])) {
+                    m_data[k] = aux[j++];
+                } else {
+                    m_data[k] = aux[i++];
+                }
+            }
+        } else {
+            return;
         }
     }
 
-    bool is_sorted(vector<T> a, int lo, int hi) {
+    bool is_sorted(std::vector<T>& a, int lo, int hi) {
         assert(!(lo < 0 || hi < 0));
         if (a.size() <= 1) {
             return true;
@@ -56,55 +51,7 @@ template <typename T> class Sort {
         return true;
     }
 
-    void sort(int lo, int hi) {
-        assert(lo >= 0 || hi >= 0);
-
-        if (hi <= lo) {
-            return;
-        };
-
-        int mid{lo + (hi - lo) / 2};
-        --hi;
-        sort(lo, mid);
-        sort(mid + 1, hi);
-        merge(lo, mid, hi);
-    }
-
-  public:
-    Sort() = default;
-
-    Sort(vector<T> other) {
-        m_data = other;
-        aux = other;
-    }
-
-    std::size_t size() const noexcept { return m_data.size; }
-
-    bool empty() const noexcept { return m_data.empty(); }
-
     bool less(const T& lhs, const T& rhs) { return compare(lhs, rhs) < 0; }
-
-    // void merge_sort() { sort_v1(); }
-
-    void merge_sort() { sort(0, m_data.size()); }
-    void resize(int size) {
-        assert(size >= 0);
-        m_data.resize(static_cast<std::size_t>(size));
-        aux.resize(static_cast<std::size_t>(size));
-    }
-
-    bool is_sorted() {
-        if (m_data.size() <= 1) {
-            return true;
-        }
-        for (std::size_t i{1}; i < m_data.size(); ++i) {
-            if (less(m_data[i - 1], m_data[i])) {
-                return false;
-            }
-        }
-
-        return true;
-    }
 
     int compare(const T& lhs, const T& rhs) {
         if (lhs < rhs) {
@@ -116,6 +63,51 @@ template <typename T> class Sort {
         }
     }
 
+    void sort(int lo, int hi) {
+
+        if (hi <= lo) {
+            return;
+        };
+
+        int mid{lo + (hi - lo) / 2};
+        sort(lo, mid);
+        sort(mid + 1, hi);
+        merge(lo, mid, hi);
+    }
+
+  public:
+    Sort() = default;
+
+    Sort(std::vector<T>& other) : m_data{other}, aux(other.size()) {}
+
+    std::size_t size() const noexcept { return m_data.size(); }
+
+    bool empty() const noexcept { return m_data.empty(); }
+    void merge_sort() {
+        if (!m_data.empty()) {
+            sort(0, m_data.size() - 1);
+        }
+    }
+
+    void resize(int size) {
+        assert(size >= 0);
+        m_data.resize(static_cast<std::size_t>(size));
+        aux.resize(static_cast<std::size_t>(size));
+    }
+
+    bool is_sorted() {
+        if (m_data.size() <= 1) {
+            return true;
+        }
+        for (std::size_t i{1}; i < m_data.size(); ++i) {
+            if (less(m_data[i], m_data[i - 1])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     void print() {
         for (auto& item : m_data) {
             std::cout << item << "\n";
@@ -124,7 +116,11 @@ template <typename T> class Sort {
 };
 
 int main() {
-    vector<int> unsorted_data{5, 6, 3, 4, 6, 3, 9, 2};
+    std::size_t size{10000000};
+    std::vector<int> unsorted_data(size);
+    for (std::size_t i{}; i < size; ++i) {
+        unsorted_data[i] = Random::get(0, size);
+    }
     Sort<int> data{unsorted_data};
     data.merge_sort();
     data.print();
