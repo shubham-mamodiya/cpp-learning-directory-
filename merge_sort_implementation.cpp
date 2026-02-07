4ac4057f0e131209afc70ce2c8c8339c97fd3f90
Shubham Mamodiya <shubham.mamodiya.dev@gmail.com>
2026-02-07 18:58:31 +0530

    improved merge sort with selection sort on some cutoff

--- merge_sort_implementation.cpp
+++ merge_sort_implementation.cpp
@@ -11,31 +11,29 @@ template <typename T> class Sort {
                   "!Sort object only intended to work on premetive data types.");
 
   private:
-    std::vector<T> m_data{};
+    std::vector<T> data{};
     std::vector<T> aux{};
-    const int CUTOFF{7};
+
+    static constexpr std::size_t cutoff{7};
 
     void merge(int lo, int mid, int hi) {
-        if (lo >= 0 && hi < static_cast<int>(m_data.size()) && lo <= hi) {
+        assert(lo >= 0 && hi < static_cast<int>(data.size()) && lo <= hi);
 
-            for (int k = lo; k <= hi; k++) {
-                aux[k] = m_data[k];
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                aux[k] = data[j++];
+            } else if (j > hi) {
+                aux[k] = data[i++];
+            } else if (less(data[j], data[i])) {
+                aux[k] = data[j++];
+            } else {
+                aux[k] = data[i++];
             }
+        }
 
-            int i = lo, j = mid + 1;
-            for (int k = lo; k <= hi; k++) {
-                if (i > mid) {
-                    m_data[k] = aux[j++];
-                } else if (j > hi) {
-                    m_data[k] = aux[i++];
-                } else if (less(aux[j], aux[i])) {
-                    m_data[k] = aux[j++];
-                } else {
-                    m_data[k] = aux[i++];
-                }
-            }
-        } else {
-            return;
+        for (int i = lo; i <= hi; ++i) {
+            data[i] = aux[i];
         }
     }
 
@@ -64,22 +62,23 @@ template <typename T> class Sort {
         }
     }
 
-    void insertion_sort(int lo, int hi) {
+    void selection_sort(int lo, int hi) {
         int smallest{};
         for (int i{lo}; i <= hi; ++i) {
             smallest = i;
             for (int j{i}; j <= hi; ++j) {
-                if (less(m_data[j], m_data[i])) {
+                if (less(data[j], data[smallest])) {
                     smallest = j;
                 }
             }
-            std::swap(m_data[i], m_data[smallest]);
+            std::swap(data[i], data[smallest]);
         }
     }
+
     void sort(int lo, int hi) {
 
         if (hi <= lo + CUTOFF - 1) {
-            insertion_sort(lo, hi);
+            selection_sort(lo, hi);
             return;
         }
 
@@ -92,29 +91,30 @@ template <typename T> class Sort {
   public:
     Sort() = default;
 
-    Sort(std::vector<T>& other) : m_data{other}, aux(other.size()) {}
+    Sort(std::vector<T>& other) : data{other}, aux(other.size()) {}
 
-    std::size_t size() const noexcept { return m_data.size(); }
+    std::size_t size() const noexcept { return data.size(); }
 
-    bool empty() const noexcept { return m_data.empty(); }
+    bool empty() const noexcept { return data.empty(); }
+
     void merge_sort() {
-        if (!m_data.empty()) {
-            sort(0, m_data.size() - 1);
+        if (!data.empty()) {
+            sort(0, data.size() - 1);
         }
     }
 
     void resize(int size) {
         assert(size >= 0);
-        m_data.resize(static_cast<std::size_t>(size));
+        data.resize(static_cast<std::size_t>(size));
         aux.resize(static_cast<std::size_t>(size));
     }
 
     bool is_sorted() {
-        if (m_data.size() <= 1) {
+        if (data.size() <= 1) {
             return true;
         }
-        for (std::size_t i{1}; i < m_data.size(); ++i) {
-            if (less(m_data[i], m_data[i - 1])) {
+        for (std::size_t i{1}; i < data.size(); ++i) {
+            if (less(data[i], data[i - 1])) {
                 return false;
             }
         }
@@ -123,14 +123,14 @@ template <typename T> class Sort {
     }
 
     void print() {
-        for (auto& item : m_data) {
+        for (auto& item : data) {
             std::cout << item << "\n";
         }
     }
 };
 
 int main() {
-    std::size_t size{10};
+    std::size_t size{20};
     std::vector<int> unsorted_data(size);
     for (std::size_t i{}; i < size; ++i) {
         unsorted_data[i] = Random::get(0, size);
