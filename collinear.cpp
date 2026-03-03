df9a5beb99cbf599b8506afe5189b88ab1f474f1
Shubham Mamodiya <shubham.mamodiya.dev@gmail.com>
2026-03-03 21:58:13 +0530

    testing if things works

--- collinear.cpp
+++ collinear.cpp
@@ -1,11 +1,28 @@
-#include "collinear_points.h"
+#include "points.h"
 
+#include <iomanip> // for setprecision
 #include <iostream>
 
+std::ostream& operator<<(std::ostream& out, const Point_2d& p) {
+    return out << std::fixed << std::setprecision(2) << "Point(" << p.m_x << ", " << p.m_y << ")";
+}
+
+std::istream& operator>>(std::istream& in, Point_2d& point) {
+
+    double x{};
+    double y{};
+
+    in >> x >> y;
+    point = in ? Point_2d{x, y} : Point_2d{};
+
+    return in;
+}
+
 int main() {
-    Point p1;
-
-    auto print_point = [=](Point& point) -> void {};
-
+    [[maybe_unused]] Point_2d p1{4.5, 5.4};
+    [[maybe_unused]] Point_2d p2{p1};
+    [[maybe_unused]] Point_2d p3 = p2;
+    std::cin >> p3;
+    std::cout << p1 << "\n" << p3;
     return 0;
 }
